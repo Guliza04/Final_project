f27a0a7467adc4038ce28f9982c5d2ac8900ca96
Guliza04 <108957074+Guliza04@users.noreply.github.com>
2022-12-13 14:55:41 +0600

    swiper_Guliza

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FirstSlider from './pages/mainPage/mainPageComponents/sliders/firstSlider/FirstSlider';
+
+function App() {
+  return (
+    <div className="App">
+    <FirstSlider/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/formPages/formSignUp/FormSignUp.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from './FormSignUp.module.css'
+
+
+
+function FormSignUp() {
+    
+  return (
+    <div>FormSignUp</div>
+  )
+
+}
+
+export default FormSignUp
